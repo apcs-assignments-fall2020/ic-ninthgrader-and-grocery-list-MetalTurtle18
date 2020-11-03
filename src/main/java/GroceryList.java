455a1c46f0c2f166a2284929e5e353d1f79605aa
MetalTurtle <dekolis1218@gmail.com>
2020-11-03 09:19:35 -0800

    Create GroceryList.java

--- src/main/java/GroceryList.java
+++ src/main/java/GroceryList.java
@@ -1,3 +1,22 @@
+import java.util.ArrayList;
+
 public class GroceryList {
-    
+    private ArrayList<String> list;
+
+    public GroceryList() {
+        this.list = new ArrayList<>();
+    }
+
+    public void add(String item) {
+        this.list.add(item);
+    }
+
+    public void remove(String item) {
+        this.list.remove(item);
+    }
+
+    @Override
+    public String toString() {
+        return "Grocery List: " + this.list.toString().substring(1, this.list.toString().length()-1);
+    }
 }
