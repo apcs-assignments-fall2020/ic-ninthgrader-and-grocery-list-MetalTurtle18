ef6c541eb1862ae9dcce66d6814ab9b99cd386af
MetalTurtle <dekolis1218@gmail.com>
2020-11-03 08:28:26 -0800

    Create NinthGrader class

--- src/main/java/NinthGrader.java
+++ src/main/java/NinthGrader.java
@@ -1,3 +1,20 @@
-public class NinthGrader {
-    
+public class NinthGrader extends Student {
+
+    public NinthGrader() {
+        super();
+    }
+
+    public NinthGrader(String a, String b) {
+        super(a, b, 2024);
+    }
+
+    @Override
+    public void doSomething() {
+        System.out.println("I am preparing for a Harkness discussion!");
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " (Ninth Grader)";
+    }
 }
--- src/main/java/NinthGraderClient.java
+++ src/main/java/NinthGraderClient.java
@@ -1,3 +1,9 @@
 public class NinthGraderClient {
-    
+
+    public static void main(String[] args) {
+        NinthGrader jerry = new NinthGrader("Jerry", "Bob");
+        System.out.println(jerry);
+        jerry.doSomething();
+    }
+
 }
